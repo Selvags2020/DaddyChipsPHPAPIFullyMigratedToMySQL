3d2df5ef3eed48fdd1535971ed4cb1582714f8ed
Selvags2020 <38200687+Selvags2020@users.noreply.github.com>
2025-10-31 17:05:23 +0530

    ready to be deployed for api

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "daddychips";
+    private $username = "root";
+    private $password = "Ecomm@13&";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+            
+            if ($this->conn->connect_error) {
+                throw new Exception("Connection failed: " . $this->conn->connect_error);
+            }
+            
+            $this->conn->set_charset("utf8");
+        } catch (Exception $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
